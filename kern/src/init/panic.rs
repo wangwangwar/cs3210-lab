56ffb9145b37745a20ace183cc46952512b9c281
wangwangwar <wangwangwar@gmail.com>
2022-03-13 14:37:54 +0800

    lab 3 phase 1: panic

--- kern/src/init/panic.rs
+++ kern/src/init/panic.rs
@@ -1,6 +1,12 @@
 use core::panic::PanicInfo;
+use crate::console::kprintln;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+   kprintln!("---------- PANIC ----------\n");
+   let loc = _info.location().expect("location");
+   kprintln!("FILE: {}\nLINE: {}\nCOL: {}", loc.file(), loc.line(), loc.column());
+   let message = _info.message().expect("message");
+   kprintln!("\n{}", message);
+   loop {}
 }
